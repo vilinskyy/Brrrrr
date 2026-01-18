4b424877d7c5928169315d022033b923f7242e80
Alexander Vilinskyy <vilinskyy7@gmail.com>
2026-01-18 09:08:56 +0000

    Initial Commit

---
+++ Brrrr/BrrrrApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrrrrApp.swift
+//  Brrrr
+//
+//  Created by Oleksandr Vilinskyi on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BrrrrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
